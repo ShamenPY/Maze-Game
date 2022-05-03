1fd7626670548febc67ef90c9eb2f2451fb77e7b
ShamenPY <karolswwacha17@gmail.com>
2022-05-03 21:26:57 +0200

    Has been added provisional walls (black lines), and mouse.

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import pygame
 BLACK = (0,0,0)
 colors = ['black']
+mouse = pygame.image.load("mouse.gif")
 
 
 class App():
 
   def __init__(self):
-    self.x = 30
-    self.y = 30
+    self.mouseX = 50
+    self.mouseY = 50
     self.main()
+    self.rect = mouse.get_rect(center=(self.mouseX/2,self.mouseY/2))
+
 
 
 
@@ -17,13 +20,12 @@ class App():
     background_colour = (255,255,255)
     self.screen.fill(background_colour)
     # pygame.draw.line(self.screen, BLACK, [670, 540], [10, 0], 5)
-    pygame.draw.line(self.screen, BLACK, [0, 457], [71, 457], 5)
-    pygame.draw.line(self.screen, BLACK, [123, 540], [123, 477], 5) # linia w dol
 
+    pygame.draw.line(self.screen, BLACK, [0, 457], [71, 457], 5)
+    pygame.draw.line(self.screen, BLACK, [123, 540], [123, 477], 5)
     pygame.draw.line(self.screen, BLACK, [174, 457], [259, 457], 5)
     pygame.draw.line(self.screen, BLACK, [174, 540], [123, 477], 5)
     pygame.draw.line(self.screen, BLACK, [222, 457], [222, 500], 5)
-
     pygame.draw.line(self.screen, BLACK, [317, 457], [388, 457], 5)
     pygame.draw.line(self.screen, BLACK, [458, 457], [670, 457], 5)
     pygame.draw.line(self.screen, BLACK, [123, 150], [123, 380], 5)
@@ -37,54 +39,25 @@ class App():
     pygame.draw.line(self.screen, BLACK, [123, 180], [71, 180], 5)
     pygame.draw.line(self.screen, BLACK, [80, 230], [123, 310], 5)
     pygame.draw.line(self.screen, BLACK, [220, 377], [190, 377], 5)
-    color = (255,0,0)
-    player = pygame.draw.rect(self.screen, color, (self.x, self.y, 60, 60))
-    sprites = pygame.sprite.Group()
-    objects = pygame.sprite.Group()
-    for sprite in objects:
-      sprite.mask = pygame.mask.from_threshold(self.screen, pygame.Color('black'), (1, 1, 1, 255))
 
-    if pygame.sprite.spritecollideany(player, objects, pygame.sprite.collide_mask):
-      print("D")
-
-
-
-
+    self.screen.blit(mouse,(self.mouseX,self.mouseY))
+    # collide = pygame.Rect.colliderect(mouse, colors)
 
 
   def create_screen(self):
     (width, height) = (670, 540)
     self.screen = pygame.display.set_mode((width, height))
-
-
-
     pygame.display.flip()
-
 
 
   def main(self):
 
-
     self.create_screen()
 
     running = True
-
-
-
-
-
-
-
-
-
-
-
     while running:
 
-
       self.draw_maze()
-
-      print(self.y)
 
 
       pygame.display.flip()
@@ -97,17 +70,20 @@ class App():
         if event.type == pygame.MOUSEBUTTONUP:
           pos = pygame.mouse.get_pos()
           print(pos)
+
+                  
+      for event in ev:
         if event.type == pygame.KEYDOWN:
           if event.key == pygame.K_LEFT:
-            self.x -= 4
+            self.mouseX -= 4
           if event.key == pygame.K_RIGHT:
-            self.x += 4
+            self.mouseX += 4
           if event.key == pygame.K_UP:
-            self.y -= 4
+            self.mouseY -= 4
           if event.key == pygame.K_DOWN:
-            self.y += 4
-      pygame.display.flip()
+            self.mouseY += 4
 
+      pygame.display.flip()
 
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
